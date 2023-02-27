48ac5671805b34b1959a433d03aff77aa318ffe9
fedeloxe <loxeee@gmail.com>
2023-02-27 17:16:05 +0100

    show done

---
+++ resources/views/movies/show.blade.php
@@ -0,0 +1,36 @@
+@extends('layouts.app')
+@section('content')
+<div class="container mt-5">
+    <div class="row">
+        <div class="col-12">
+            <div class="d-flex">
+                {{-- locandina film --}}
+                <img class="w-25 rounded-1" src=""> 
+                <div class="info ms-5">
+                    {{-- titolo film --}}
+                    <h4></h4>
+                    {{-- titolo originale --}}
+                    <h5></h5>
+                    {{-- nazione --}}
+                    <h5></h5>
+                    {{-- relase date --}}
+                    <h5></h5>
+                    {{-- vote --}}
+                    <h5></h5>
+                    {{-- cast --}}
+                    <p></p>
+                    <div class="d-flex">
+                        <h3></h3>
+                        <a href="" class="btn btn-warning btn-square ms-2"><i class="fa-solid fa-cart-shopping"></i></a>
+                    </div>
+                    <p></p>
+                </div>
+            </div>
+        </div>
+        <div class="edit my-5">
+            <a href="" class="btn btn-info btn-square"><i class="fa-solid fa-pen"></i></a>
+
+
+        </div>
+    </div>
+</div>
