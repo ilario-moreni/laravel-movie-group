e362d63202b76dc0e1fc31f0471cbbebeb65a4e2
ilario-moreni <ilario2001.mor@gmail.com>
2023-03-02 15:35:33 +0100

    add and seed db

--- resources/views/movies/index.blade.php
+++ resources/views/movies/index.blade.php
@@ -1,24 +1,24 @@
-@extends('layouts.app')
+@extends('layout.app')
 @section('content')
-<div class="container">
-    <div class="container d-flex">
-        <a href=""><button type="button" class="btn btn-primary mt-2">Aggiungi un nuovo Film</button></a>
-    </div>
-    <h2 class="my-4"> Film </h2>
-    <div class="row">
-        @foreach ( )
-            <div class="col-4 my-2">
-                <a href="">
-                    <div class="card">
-                        <img class="card-img-top img-fluid" src="" >
-                        <div class="card-body">
-                            <h5></h5>
-                          
+    <div class="container">
+        <div class="container d-flex">
+            <a href=""><button type="button" class="btn btn-primary mt-2">Aggiungi un nuovo Film</button></a>
+        </div>
+        <h2 class="my-4"> Film </h2>
+        <div class="row">
+            @foreach ($movies as $movie)
+                <div class="col-4 my-2">
+                    <a href="">
+                        <div class="card">
+                            <img class="card-img-top img-fluid" src="">
+                            <div class="card-body">
+                                <h5>{{ $movie->title }}</h5>
+
+                            </div>
                         </div>
-                    </div>
-                </a>
-            </div>
-        @endforeach
+                    </a>
+                </div>
+            @endforeach
+        </div>
     </div>
-</div>
 @endsection
